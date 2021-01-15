8fffd5c75f8b054ee2ba2496a1fd41e8900e7c58
coder-19-19 <76615028+coder-19-19@users.noreply.github.com>
2021-01-15 17:45:49 +0400

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const cors = require("cors")
 const ytdl = require("ytdl-core")
 const app = express()
 
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 8080
 
 app.use(cors())
 
